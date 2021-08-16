1de6730450571ee1825c82900a187ae7baab121a
David <daveman@gmail.com>
2021-08-16 14:18:08 -0700

    🔄 SYNC: updated model columns for dateTime

--- react-app/src/components/SpotPage/SpotPage.js
+++ react-app/src/components/SpotPage/SpotPage.js
@@ -2,15 +2,18 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchOneSpot } from "../../store/spots";
+import { fetchMedia, getOneMedium } from "../../store/media";
 
 const SpotPage = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
   useEffect(() => {
     dispatch(fetchOneSpot(id));
+    dispatch(fetchMedia(id));
   }, [dispatch]);
 
   const spot = useSelector((state) => state.spotReducer.spots[0]);
+  const media = useSelector((state) => state.mediaReducer.media);
 
   return (
     <div>
@@ -19,7 +22,10 @@ const SpotPage = () => {
           <div>{spot.name}</div>
           <img src={spot.imageUrl} alt={spot.name}></img>
           <div>{spot.address}</div>
-          <div>{spot.description}</div>
+					<div>{spot.description}</div>
+					<div>{media.map((medium) => (
+						<img src={medium.mediaUrl} alt={medium.description} />
+					))}</div>
         </div>
       ) : (
         <h1>404 Spot Not Found</h1>
---
+++ react-app/src/store/media.js
@@ -0,0 +1,46 @@
+//setup constants
+const GET_MEDIA = 'media/getMedia';
+const GET_ONE_MEDIUM = 'media/getOneMedium';
+
+//action creators
+export const getMedia = (media) => {
+	return { type: GET_MEDIA, media };
+}
+
+export const getOneMedium = (medium) => {
+	return { type: GET_ONE_MEDIUM, medium };
+}
+
+//thunk creator
+export const fetchMedia = (id) => async (dispatch) => {
+	const res = await fetch(`/api/media/spots/${id}`);
+
+	if (res.ok) {
+		const media = await res.json();
+		dispatch(getMedia(media));
+	}
+}
+export const fetchOneMedium = (id) => async (dispatch) => {
+	const res = await fetch(`/api/media/${id}`);
+
+	if (res.ok) {
+		const medium = await res.json();
+		dispatch(getOneMedium(medium));
+	}
+}
+
+//initialState
+const initialState = { media: [] };
+
+const mediaReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case GET_MEDIA:
+			return {...state, ...action.media };
+		case GET_ONE_MEDIUM:
+			return {...state, ...action.medium };
+		default:
+			return state;
+	}
+}
+
+export default mediaReducer;
