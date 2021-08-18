20dbebbc03f4101d5531fb3b34c2ac0132742cda
Owen Iwamasa <owiwamasa@gmail.com>
2021-08-17 22:12:44 -0400

    styled navbar and login form

--- react-app/src/components/login_modal/LoginForm.js
+++ react-app/src/components/login_modal/LoginForm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
+import './loginform.css'
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
@@ -31,34 +32,42 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
-      </div>
-    </form>
+    <div className='form-div'>
+      <form onSubmit={onLogin}>
+        <div className='form-content'>
+        <div className='form-all-inputs-container'>
+          <div className='form-h3-container'>
+              <h3 className='form-h3'>Log In</h3>
+          </div>
+        <div className='form-input-container'>
+          <label className='form-label' htmlFor='email'>Email</label>
+          <input
+            className='form-input'
+            name='email'
+            type='text'
+            placeholder='Email'
+            value={email}
+            onChange={updateEmail}
+          />
+        </div>
+        <div className='form-input-container'>
+          <label className='form-label' htmlFor='password'>Password</label>
+          <input
+            className='form-input'
+            name='password'
+            type='password'
+            placeholder='Password'
+            value={password}
+            onChange={updatePassword}
+          />
+        </div>
+        </div>
+          <div className='form-submit-btn'>
+              <button className='form-btn' type='submit'>Login</button>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 };
 
