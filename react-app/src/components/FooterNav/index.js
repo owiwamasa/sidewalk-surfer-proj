8def4d370a69eac0005ff3048054ece816073bd3
David <daveman@gmail.com>
2021-08-19 16:01:45 -0700

    🔄 SYNC: WIP - footer base templates

---
+++ react-app/src/components/FooterNav/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
+import './FooterNav.css'
+
+const FooterNav = () => {
+  return (
+    <div className='footer-container'>
+      <div>
+        test render footer
+      </div>
+    </div>
+  );
+}
+
+export default FooterNav;
