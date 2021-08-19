7455fbd89ff8d8de9c6e16434662abd34d249df2
Owen Iwamasa <owiwamasa@gmail.com>
2021-08-19 18:05:59 -0400

    added spaces

--- react-app/src/store/comments.js
+++ react-app/src/store/comments.js
@@ -3,7 +3,6 @@ const GET_COMMENTS = "comments/getComments";
 const GET_ONE_COMMENT = "comments/getOneComment";
 const POST_ONE_COMMENT = "comments/postOneComment";
 const EDIT_COMMENT = "comments/editComment"
-const DELETE_COMMENT = "comments/deleteComment"
 
 //action creators
 export const getComments = (comments) => {
@@ -16,32 +15,29 @@ export const getOneComment = (comment) => {
 
 export const postOneComment = (comment) => {
   return { type: POST_ONE_COMMENT, comment };
-};
-
-const editComment = (comment) => {
-  return { type: EDIT_COMMENT, comment };
-};
-
-const deleteComment = (commentId) => {
-  return { type: DELETE_COMMENT, commentId };
 }
+
+const editComment = (comment) =>{
+  return {type: EDIT_COMMENT, comment};
+}
+
 
 //thunk creator
 export const postComment = (comment) => async (dispatch) => {
-  const res = await fetch(`/api/comments`, {
+  const res = await fetch (`/api/comments/`, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(comment),
-  });
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify(comment)
+  })
   if (res.ok) {
     const comment = await res.json();
     dispatch(postOneComment(comment));
   }
   return comment;
-};
+}
 
 export const fetchComments = () => async (dispatch) => {
-  const res = await fetch(`/api/comments`);
+  const res = await fetch(`/api/comments/`);
 
   if (res.ok) {
     const comments = await res.json();
@@ -58,26 +54,16 @@ export const fetchOneComment = (id) => async (dispatch) => {
   }
 };
 
-export const editOneComment = (comment, id) => async (dispatch) => {
+export const editOneComment = (comment,id) => async (dispatch) => {
   const res = await fetch(`/api/comments/${id}`, {
     method: "PUT",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(comment),
-  });
+    headers: { 'Content-Type': 'application/json'},
+    body: JSON.stringify(comment)
+  })
 
   if (res.ok) {
-    const comment = await res.json();
+    const comment = await res.json()
     dispatch(editComment(comment));
-  }
-};
-
-export const deleteOneComment = (id) => async (dispatch) => {
-  const res = await fetch(`/api/comments/${id}`, {
-    method: 'DELETE'
-  });
-
-  if (res.ok) {
-    dispatch(deleteComment(id));
   }
 }
 
@@ -91,17 +77,9 @@ const commentReducer = (state = initialState, action) => {
     case GET_ONE_COMMENT:
       return { ...state, ...action.comment };
     case POST_ONE_COMMENT:
-      return { ...state, comments: [...state.comments, action.comment] };
+      return { ...state, comments: [action.comment, ...state.comments] };
     case EDIT_COMMENT:
-      // return {...state, comments:[action.comment, ...state.comments]}
-      return { ...state, ...action.comment }
-    case DELETE_COMMENT:
-      return {
-        ...state,
-        comments: [
-          ...state.comments.filter((comment) => comment.id !== action.commentId),
-        ],
-      }
+      return {...state, comments: [action.comment, ...state.comments] }
     default:
       return state;
   }
