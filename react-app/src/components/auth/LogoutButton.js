d6a177d1e3117aeb9f7003d00a59b7e987e6183a
Owen Iwamasa <owiwamasa@gmail.com>
2021-08-20 17:34:06 -0400

    started search bar

--- react-app/src/components/auth/LogoutButton.js
+++ react-app/src/components/auth/LogoutButton.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router';
 import { logout } from '../../store/session';
 
 const LogoutButton = () => {
+  const history = useHistory();
   const dispatch = useDispatch()
   const onLogout = async (e) => {
     await dispatch(logout());
+    history.push('/');
   };
 
   return <button id='logout-btn' onClick={onLogout}>Logout</button>;
