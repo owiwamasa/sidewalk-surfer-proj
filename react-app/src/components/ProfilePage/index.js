d149bb279c7dfc954d4cc1198f606cca84de48a3
Conner U <cunderhillosx@gmail.com>
2021-08-20 09:58:42 -0700

    video thumbnail

--- react-app/src/components/ProfilePage/index.js
+++ react-app/src/components/ProfilePage/index.js
@@ -1,21 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { fetchSpots } from '../../store/spots'
-import { fetchAllMedia } from '../../store/media'
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchSpots } from "../../store/spots";
+import { fetchAllMedia } from "../../store/media";
 import { fetchComments } from "../../store/comments";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 import MediaModal from "../MediaModal";
 
-
-import './ProfilePage.css'
-
+import "./ProfilePage.css";
 
 function ProfilePage() {
   const [user, setUser] = useState({});
-  const { userId }  = useParams();
+  const { userId } = useParams();
 
-  const dispatch =useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (!userId) {
@@ -26,70 +24,51 @@ function ProfilePage() {
       const user = await response.json();
       setUser(user);
     })();
-    dispatch(fetchSpots())
-    dispatch(fetchAllMedia())
+    dispatch(fetchSpots());
+    dispatch(fetchAllMedia());
     dispatch(fetchComments());
+  }, [userId, dispatch]);
 
-  }, [userId,dispatch]);
+  function convertLink(m) {
+    let url = m.mediaUrl;
+    if (m?.mediaUrl.includes("youtube")) {
+      url = m.mediaUrl.split("watch?v=");
+      url = `http://img.youtube.com/vi/${url[1]}/0.jpg`;
+    }
+    return url;
+  }
 
   const spots = useSelector((state) => state.spotReducer.spots);
   const media = useSelector((state) => state.mediaReducer.media);
   const comments = useSelector((state) => state.commentReducer.comments);
-  console.log(comments)  
+  console.log(comments);
   if (!user) {
     return null;
   }
   return (
     <div>
-      <div className='top'>
-        <div className='top_left'>
-          <img src={user.profilepic} alt=""/>
+      <div className="top">
+        <div className="top_left">
+          <img src={user.profilepic} alt="" />
         </div>
-        <div className='top_right'>
-          <div className='un'>
-            {user.username}
-          </div>
-          <div className='email'>
-            Email: {user.email}
-          </div>
+        <div className="top_right">
+          <div className="un">{user.username}</div>
+          <div className="email">Email: {user.email}</div>
         </div>
       </div>
-      {/* <div>
-        Spots:
-        {spots.map((spot) => (
-          <div key={spot.id}>
-            {user?.id === spot?.userId ?
-            <div>
-              <img src={spot.imageUrl} alt=""></img>
-              <div>
-                {spot.name}
-              </div>
-              <div>
-                {spot.address}
-              </div>
-              <div>
-                {spot.description}
-              </div>
-            </div>
-            : null
-            }
-          </div>
-        ))}
-      </div> */}
-      <div className='whole'>
-        <div className='grid-container' >
+
+      <div className="whole">
+        <div className="grid-container">
           {media.map((m) => (
             <div key={m.id}>
-              {user?.id === m?.userId ?
-              <div className='spotdiv'>
-                <img src={m.mediaUrl} alt=""></img>
-                <div className='modal'>
-                <MediaModal media={m} comments={comments}>
-                </MediaModal>
+              {user?.id === m?.userId ? (
+                <div className="spotdiv">
+                  <img src={convertLink(m)} alt=""></img>
+                  <div className="modal">
+                    <MediaModal media={m} comments={comments}></MediaModal>
+                  </div>
                 </div>
-              </div>
-              : null
-            }
+              ) : null}
             </div>
           ))}
         </div>
