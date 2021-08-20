c548ba01540cb5d5b4ec06d07bce17deaaf5c2a8
David <daveman@gmail.com>
2021-08-20 14:44:00 -0700

    🎨 STYLE: completed footer

--- react-app/src/components/FooterNav/index.js
+++ react-app/src/components/FooterNav/index.js
@@ -8,52 +8,80 @@ const FooterNav = () => {
     <>
       <div className='footer-container'>
         <div className='footer-links-container'>
+              <div className='footer-title'>Development Team</div>
           <div className='profile-images-small'>
-            <div>
-              <img src='https://i.imgur.com/7XFqUbN.jpg' alt='Conner' width="75" height="75" />
-              &nbsp;
-              <a href="mailto: cunderhillosx@gmail.com" target="_blank" rel="noreferrer">
-                <i class="far fa-envelope" />
-              </a>
-              &nbsp;
-              <a href="https://github.com/conneru" target="_blank" rel="noreferrer">
-                <i class="fab fa-github" />
-              </a>
-              &nbsp;
-              <a href="https://www.linkedin.com/in/conner-underhill-64b3921a2" target="_blank" rel="noreferrer">
-                <i class="fab fa-linkedin" />
-              </a>
+            <div className='footer-box'>
+              <div className='footer-div'>
+                <div className="footer-name">Conner Underhill</div>
+                  <div className="footer-links">
+                    <a href="mailto:cunderhillosx@gmail.com" target="_blank" rel="noreferrer">
+                      <i class="far fa-envelope" />
+                    </a>
+                    &nbsp;
+                    <a href="https://github.com/conneru" target="_blank" rel="noreferrer">
+                      <i class="fab fa-github" />
+                    </a>
+                    &nbsp;
+                    <a href="https://www.linkedin.com/in/conner-underhill-64b3921a2" target="_blank" rel="noreferrer">
+                      <i class="fab fa-linkedin" />
+                    </a>
+                  </div>
+                </div>
 
-              <img src='https://i.imgur.com/JKb72HY.jpg' width="75" height="75" alt='David' />
-              &nbsp;
-              <a href="mailto: daveman@gmail.com" target="_blank" rel="noreferrer">
-                <i class="far fa-envelope" />
-              </a>
-              &nbsp;
-              <a href="https://www.linkedin.com/in/david-h-374b30154/" target="_blank" rel="noreferrer">
-                <i class="fab fa-github" />
-              </a>
-              &nbsp;
-              <a href="https://github.com/deardaveed" target="_blank" rel="noreferrer">
-                <i class="fab fa-linkedin" />
-              </a>
+              <div className='footer-div'>
+                <div className="footer-name">David Huang</div>
+                  <div className="footer-links">
+                    <a href="mailto:daveman@gmail.com" target="_blank" rel="noreferrer">
+                      <i class="far fa-envelope" />
+                    </a>
+                    &nbsp;
+                    <a href="https://www.linkedin.com/in/david-h-374b30154/" target="_blank" rel="noreferrer">
+                      <i class="fab fa-github" />
+                    </a>
+                    &nbsp;
+                    <a href="https://github.com/deardaveed" target="_blank" rel="noreferrer">
+                      <i class="fab fa-linkedin" />
+                    </a>
+                  </div>
+              </div>
 
-              <img src='https://i.imgur.com/fY9zGrA.jpg' width="75" height="75" alt='John' />
-              &nbsp;<a href="mailto: cunderhillosx@gmail.com" target="_blank" rel="noreferrer"></a><i class="far fa-envelope" />&nbsp;<i class="fab fa-github"></i>&nbsp;<i class="fab fa-linkedin" />
+              <div className='footer-div'>
+                <div className="footer-name">John Wanis</div>
+                  <div className="footer-links">
+                    <a href="mailto:john.wanis@yahoo.com" target="_blank" rel="noreferrer">
+                      <i class="far fa-envelope" />
+                    </a>
+                    &nbsp;
+                    <a href="https://www.linkedin.com/in/john-wanis-764957138/" target="_blank" rel="noreferrer">
+                      <i class="fab fa-github" />
+                    </a>
+                    &nbsp;
+                    <a href="https://github.com/Jomix-13" target="_blank" rel="noreferrer">
+                      <i class="fab fa-linkedin" />
+                    </a>
+                  </div>
+              </div>
 
-              <img src='https://i.imgur.com/4ZiRI4P.jpg' width="75" height="75" alt='Owen' />
-              &nbsp;<a href="mailto: cunderhillosx@gmail.com" target="_blank" rel="noreferrer"></a><i class="far fa-envelope" />&nbsp;<i class="fab fa-github"></i>&nbsp;<i class="fab fa-linkedin" />
+              <div className='footer-div'>
+                <div className="footer-name">Owen Iwamasa</div>
+                  <div className="footer-links">
+                    <a href="mailto:owiwamasa@gmail.com" target="_blank" rel="noreferrer">
+                      <i class="far fa-envelope" />
+                    </a>
+                    &nbsp;
+                    <a href="https://www.linkedin.com/in/owen-iwamasa-6ab3a9166/" target="_blank" rel="noreferrer">
+                      <i class="fab fa-github" />
+                    </a>
+                    &nbsp;
+                    <a href="https://github.com/owiwamasa" target="_blank" rel="noreferrer">
+                      <i class="fab fa-linkedin" />
+                    </a>
+                  </div>
+                </div>
               </div>
           </div>
         </div>
       </div>
-      <div className='footers-footer-container'>
-        <div className='footer-data'>Developed by: Conner Underhill | David Huang | John Wanis | Owen Iwamasa</div>
-        <div className='footer-data'>TEST ROW 2
-
-        </div>
-      </div>
-
     </>
   );
 }
