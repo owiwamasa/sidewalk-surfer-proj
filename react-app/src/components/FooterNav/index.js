4dc218526acb232ee4de2de8e55582548ce12e5b
David <daveman@gmail.com>
2021-08-20 11:06:12 -0700

    🔄 SYNC: footer WIP

--- react-app/src/components/FooterNav/index.js
+++ react-app/src/components/FooterNav/index.js
@@ -5,11 +5,25 @@ import './FooterNav.css'
 
 const FooterNav = () => {
   return (
-    <div className='footer-container'>
-      <div>
-        test render footer
+    <>
+      <div className='footer-container'>
+        <div className='footer-links-container'>
+         <div className='profile-images-small'>
+            <img src='https://i.imgur.com/7XFqUbN.jpg' alt='Conner' width="75" height="75"></img>
+            <img src='https://i.imgur.com/JKb72HY.jpg' width="75" height="75" alt='David'></img>
+            <img src='https://i.imgur.com/fY9zGrA.jpg' width="75" height="75" alt='John'></img>
+            <img src='https://i.imgur.com/4ZiRI4P.jpg' width="75" height="75" alt='Owen'></img>
+          </div>
+        </div>
       </div>
-    </div>
+      <div className='footers-footer-container'>
+        <div className='footer-data'>Developed by: Conner Underhill | David Huang | John Wanis | Owen Iwamasa  @App Academy 2021.</div>
+        <div className='footer-data'>
+
+        </div>
+      </div>
+
+    </>
   );
 }
 
